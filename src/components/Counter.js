9a6048647362e9bc268d1cc66ccb76e677f80900
hjcho <hjcho@seegene.com>
2021-06-29 20:40:11 +0900

    implement a logger middleware

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,7 +3,7 @@ import React from "react";
 const Counter = ({ number, onIncrease, onDecrease }) => {
   return (
     <div>
-      <h1>{number}</h1>
+      <h1>#{number}</h1>
       <button onClick={onIncrease}>+1</button>
       <button onClick={onDecrease}>-1</button>
     </div>
--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -3,6 +3,23 @@ import { connect } from "react-redux";
 import { increase, decrease } from "../modules/counter";
 import Counter from "../components/Counter";
 
+import { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// const CounterContainer = () => {
+//   const number = useSelector((state) => state.counter);
+//   const dispatch = useDispatch();
+//   return (
+//     <Counter
+//       number={number}
+//       onIncrease={useCallback(() => dispatch(increase()), [dispatch])}
+//       onDecrease={useCallback(() => dispatch(decrease()), [dispatch])}
+//     />
+//   );
+// };
+
+// export default React.memo(CounterContainer);
+
 const CounterContainer = ({ number, increase, decrease }) => {
   return (
     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
