e1e84498a164186ef13b37a2bfd10cdff9d36988
hjcho <hjcho@seegene.com>
2021-07-01 17:31:46 +0900

    use createRequestThunk()

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from 'redux-actions';
 import * as api from '../lib/api';
+import createRequestThunk from '../lib/createRequestThunk';
 
 //ACTION CONSTANT
 const GET_POST = 'sample/GET_POST';
@@ -11,27 +12,29 @@ const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 //THUNK 함수 생성
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST });
-  try {
-    const response = await api.getPost(id);
-    dispatch({ type: GET_POST_SUCCESS, payload: response.data });
-  } catch (e) {
-    dispatch({ type: GET_POST_FAILURE, payload: e, error: true });
-    throw e;
-  }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+// export const getPost = (id) => async (dispatch) => {
+//   dispatch({ type: GET_POST });
+//   try {
+//     const response = await api.getPost(id);
+//     dispatch({ type: GET_POST_SUCCESS, payload: response.data });
+//   } catch (e) {
+//     dispatch({ type: GET_POST_FAILURE, payload: e, error: true });
+//     throw e;
+//   }
+// };
 
-export const getUsers = () => async (dispatch) => {
-  dispatch({ type: GET_USERS });
-  try {
-    const response = await api.getUsers();
-    dispatch({ type: GET_USERS_SUCCESS, payload: response.data });
-  } catch (e) {
-    dispatch({ type: GET_USERS_FAILURE, payload: e, error: true });
-    throw e;
-  }
-};
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+// export const getUsers = () => async (dispatch) => {
+//   dispatch({ type: GET_USERS });
+//   try {
+//     const response = await api.getUsers();
+//     dispatch({ type: GET_USERS_SUCCESS, payload: response.data });
+//   } catch (e) {
+//     dispatch({ type: GET_USERS_FAILURE, payload: e, error: true });
+//     throw e;
+//   }
+// };
 
 //초기 상태 상수
 const initialState = {
