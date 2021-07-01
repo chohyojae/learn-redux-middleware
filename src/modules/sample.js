a4704314f0215da2804d63fd73ca3a05659f229a
hjcho <hjcho@seegene.com>
2021-07-01 17:03:23 +0900

    getPost, getUsers

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,14 +1,14 @@
-import { handleActions } from "redux-actions";
-import * as api from "../lib/api";
+import { handleActions } from 'redux-actions';
+import * as api from '../lib/api';
 
 //ACTION CONSTANT
-const GET_POST = "sample/GET_POST";
-const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
-const GET_POST_FAILURE = "sample/GET_POST_FAILURE";
+const GET_POST = 'sample/GET_POST';
+const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
+const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
-const GET_USERS = "sample/GET_USERS";
-const GET_USERS_SUCCESS = "sample/GET_POST_SUCCESS";
-const GET_USERS_FAILURE = "sample/GET_POST_FAILURE";
+const GET_USERS = 'sample/GET_USERS';
+const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
+const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 //THUNK 함수 생성
 export const getPost = (id) => async (dispatch) => {
@@ -73,7 +73,7 @@ const sample = handleActions(
       loading: { ...state.loading, GET_USERS: false },
     }),
   },
-  initialState
+  initialState,
 );
 
 export default sample;
